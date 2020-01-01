c02ac956c47a8fa77437bfaafeb77fb3a9c00085
koharu_okahana <koharu@re2.org>
2020-01-02 01:21:23 +0900

    入力したものをDBに登録する

--- app/src/main/java/com/example/accountbook/MainActivity.java
+++ app/src/main/java/com/example/accountbook/MainActivity.java
@@ -14,22 +14,30 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.contentcapture.ContentCaptureCondition;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.text.InputType;
 
-import static android.widget.Toast.*;
-
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private TestOpenHelper helper;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        textView = findViewById(R.id.text_view);
+
+
+        /* ツールバー */
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -42,22 +50,82 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        EditText purchaseText = findViewById(R.id.input_purchase);
+        EditText priceText = findViewById(R.id.input_price);
+        priceText.setInputType(InputType.TYPE_CLASS_NUMBER); //数字だけ入力させる
+
         /* 登録ボタン */
         Button register = findViewById(R.id.registration);
         register.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText purchaseText = findViewById(R.id.input_purchase);
                 String purchase = purchaseText.getText().toString();
-
-                EditText amountText = findViewById(R.id.input_amount);
-                amountText.setInputType(InputType.TYPE_CLASS_NUMBER); //数字だけ入力させる
-                String amount = amountText.getText().toString();
-                if(purchase.equals("") || amount.equals("")){
+                String price = priceText.getText().toString();
+                if(purchase.equals("") || price.equals("")){
                     Toast.makeText(MainActivity.this, "入力されていない項目があります", Toast.LENGTH_LONG).show();
                 }
-                else Toast.makeText(MainActivity.this, "登録しました", Toast.LENGTH_LONG).show();
+                else { //登録作業
+                    if(helper == null) { helper = new TestOpenHelper(getApplicationContext()); }
+                    SQLiteDatabase db = helper.getWritableDatabase();
+                    insertData(db, purchase, Integer.valueOf(price));
+                    Toast.makeText(MainActivity.this, "登録しました", Toast.LENGTH_LONG).show();
+                }
             }
         });
+
+        /* 削除ボタン */
+        Button delete = findViewById(R.id.delete);
+        delete.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "削除しました", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        /* 表示ボタン */
+        Button display = findViewById(R.id.display);
+        display.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this, "DBを表示します", Toast.LENGTH_LONG).show();
+                readData();
+            }
+        });
+    }
+
+    private void readData() {
+        if(helper == null) { helper = new TestOpenHelper(getApplicationContext()); }
+        SQLiteDatabase db = helper.getReadableDatabase();
+
+        Cursor cursor = db.query(
+                "testdb",
+                new String[] { "purchase", "price" },
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+        cursor.moveToFirst();
+
+        StringBuilder sbuilder = new StringBuilder();
+
+        for (int i = 0; i < cursor.getCount(); i++) {
+            sbuilder.append(cursor.getString(0));
+            sbuilder.append(":    ");
+            sbuilder.append(cursor.getInt(1));
+            sbuilder.append("円\n\n");
+            cursor.moveToNext();
+        }
+
+        cursor.close();
+
+        textView.setText(sbuilder.toString());
+    }
+
+    private void insertData(SQLiteDatabase db, String purchase, int price) {
+        ContentValues values = new ContentValues();
+        values.put("purchase", purchase);
+        values.put("price", price);
     }
 }
--- app/src/main/java/com/example/accountbook/TestOpenHelper.java
+++ app/src/main/java/com/example/accountbook/TestOpenHelper.java
@@ -12,7 +12,7 @@ https://qiita.com/kengo_kuwahara/items/a8ef858a9810cad42ca6
 
 public class TestOpenHelper extends SQLiteOpenHelper{
 
-    //データベースの場0ジョン
+    //データベースのバージョン
     private static final int DATABASE_VERSION = 3;
 
     //データベース情報を変数に格納
@@ -20,34 +20,29 @@ public class TestOpenHelper extends SQLiteOpenHelper{
     private static final String TABLE_NAME = "testdb";
     private static final String _ID = "_id";
     private static final String COLUMN_NAME_PURCHASE = "purchase";
-    private static final String COLUMN_NAME_AMOUNT = "amount";
+    private static final String COLUMN_NAME_PRICE = "price";
 
     private static final String SQL_CREATE_ENTRIES =
-            "CREATE TABLE" + TABLE_NAME + "(" +
-                    _ID + " INTEGER PRIMARY KEY,"+
-                    COLUMN_NAME_PURCHASE + " TEXT,"+
-                    COLUMN_NAME_AMOUNT + " INTEGER)";
+            "CREATE TABLE " + TABLE_NAME + " (" + _ID + " INTEGER PRIMARY KEY,"+
+                    COLUMN_NAME_PURCHASE + " TEXT,"+ COLUMN_NAME_PRICE + " INTEGER)";
 
-    private static final String SQL_DELETE_ENTRIES =
-            "DROP TABLE IF EXISTS " + TABLE_NAME;
+    private static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + TABLE_NAME;
 
     TestOpenHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    //テーブル作成
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(SQL_CREATE_ENTRIES);
-        saveData(db, "コロッケ2個", 60 * 2);
-        saveData(db, "頭痛薬", 1500);
-        saveData(db, "美容院", 5000);
-        saveData(db, "うどん", 100);
     }
 
+    // アップデートの判別，古いバージョンは削除して新規作成する
     @Override
     public void onUpgrade(SQLiteDatabase db, int onldVersion, int newVersion){
         db.execSQL(
-                SQL_CREATE_ENTRIES
+                SQL_DELETE_ENTRIES
         );
         onCreate(db);
     }
@@ -55,12 +50,4 @@ public class TestOpenHelper extends SQLiteOpenHelper{
     public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion){
         onUpgrade(db, oldVersion, newVersion);
     }
-
-    public void saveData(SQLiteDatabase db, String purchace, int amount){
-        ContentValues values = new ContentValues();
-        values.put("Purchace", purchace);
-        values.put("Amount", amount);
-        db.insert("testdb", null, values);
-    }
-
 }
