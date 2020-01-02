c17a4fe7a83ce59e440acded09b22a4a62d23ecf
koharu_okahana <koharu@re2.org>
2020-01-03 01:16:18 +0900

    最低要件は満たした

--- app/src/main/java/com/example/accountbook/MainActivity.java
+++ app/src/main/java/com/example/accountbook/MainActivity.java
@@ -12,6 +12,7 @@ import android.content.ContentValues;
 import android.os.Bundle;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.contentcapture.ContentCaptureCondition;
@@ -54,8 +55,8 @@ public class MainActivity extends AppCompatActivity {
         EditText priceText = findViewById(R.id.input_price);
         priceText.setInputType(InputType.TYPE_CLASS_NUMBER); //数字だけ入力させる
 
-        /* 登録ボタン */
-        Button register = findViewById(R.id.registration);
+        /* 追加ボタン */
+        Button register = findViewById(R.id.add);
         register.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -68,7 +69,28 @@ public class MainActivity extends AppCompatActivity {
                     if(helper == null) { helper = new TestOpenHelper(getApplicationContext()); }
                     SQLiteDatabase db = helper.getWritableDatabase();
                     insertData(db, purchase, Integer.valueOf(price));
-                    Toast.makeText(MainActivity.this, "登録しました", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, price+"円の"+purchase+"を登録しました", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        /* 更新ボタン */
+        Button update = findViewById(R.id.update);
+        update.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String purchase = purchaseText.getText().toString();
+                String price = priceText.getText().toString();
+                if(purchase.equals("") && price.equals("")){
+                    Toast.makeText(MainActivity.this, "入力されていない項目があります", Toast.LENGTH_LONG).show();
+                }
+                else { //更新作業
+                    if(helper == null) { helper = new TestOpenHelper(getApplicationContext()); }
+                    SQLiteDatabase db = helper.getWritableDatabase();
+                    ContentValues val = new ContentValues();
+                    val.put("price", Integer.parseInt(price));
+                    db.update("testdb", val, "purchase=?", new String[]{purchase});
+                    Toast.makeText(MainActivity.this, "更新しました", Toast.LENGTH_LONG).show();
                 }
             }
         });
@@ -78,7 +100,31 @@ public class MainActivity extends AppCompatActivity {
         delete.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "削除しました", Toast.LENGTH_LONG).show();
+                String purchase = purchaseText.getText().toString();
+                String price = priceText.getText().toString();
+                if(purchase.equals("") && price.equals("")){
+                    Toast.makeText(MainActivity.this, "入力されていない項目があります", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    if(helper == null) { helper = new TestOpenHelper(getApplicationContext()); }
+                    SQLiteDatabase db = helper.getWritableDatabase();
+                    String sql = "delete from testdb where (purchase=\'"+purchase+"\' and price=\'"+price+"\');";
+                    db.execSQL(sql);
+                    Toast.makeText(MainActivity.this, "削除しました", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        /* 全削除ボタン */
+        Button delete_all = findViewById(R.id.delete_all);
+        delete_all.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(helper == null) { helper = new TestOpenHelper(getApplicationContext()); }
+                SQLiteDatabase db = helper.getWritableDatabase();
+                String sql = "delete from testdb";
+                db.execSQL(sql);
+                Toast.makeText(MainActivity.this, "全削除しました", Toast.LENGTH_LONG).show();
             }
         });
 
@@ -114,7 +160,7 @@ public class MainActivity extends AppCompatActivity {
             sbuilder.append(cursor.getString(0));
             sbuilder.append(":    ");
             sbuilder.append(cursor.getInt(1));
-            sbuilder.append("円\n\n");
+            sbuilder.append("円\n");
             cursor.moveToNext();
         }
 
@@ -127,5 +173,6 @@ public class MainActivity extends AppCompatActivity {
         ContentValues values = new ContentValues();
         values.put("purchase", purchase);
         values.put("price", price);
+        db.insert("testdb", null, values);
     }
 }
